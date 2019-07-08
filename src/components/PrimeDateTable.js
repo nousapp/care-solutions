de0012299931e005ef860b38a9ede3bb75276d58
Devon Marantz <DMarantz@talentpath.com>
2019-07-08 11:57:48 -0700

    Add paginator

--- src/components/PrimeDateTable.js
+++ src/components/PrimeDateTable.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
+import {Button} from 'primereact/button';
 // Styles
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -17,9 +18,11 @@ class PrimeDataTable extends React.Component {
 
   render() {
     const { products } = this.props;
+    let paginatorLeft = <Button icon="pi pi-refresh"/>;
+    let paginatorRight = <Button icon="pi pi-cloud-upload"/>;
 
     return (
-      <DataTable value={products}>
+      <DataTable value={products} paginator={true} paginatorLeft={paginatorLeft} paginatorRight={paginatorRight} rows={10} rowsPerPageOptions={[5,10,20]}>
           <Column field="name" header="Name" />
           <Column field="serviceCode" header="Service Code" />
           <Column field="serviceBy" header="Serviced By" />
