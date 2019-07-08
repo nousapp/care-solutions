54257f5c2627d984fd88b8445a33b90bb427988c
Devon Marantz <DMarantz@talentpath.com>
2019-07-08 12:04:47 -0700

    Add export to CSV

--- src/components/PrimeDateTable.js
+++ src/components/PrimeDateTable.js
@@ -12,17 +12,24 @@ class PrimeDataTable extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.state = {};
+    this.export = this.export.bind(this);
+  }
+
+  export() {
+    this.dt.exportCSV();
   }
 
   render() {
     const { products } = this.props;
+    // Export To CSV
+    var header = <div style={{textAlign:'left'}}><Button type="button" icon="pi pi-external-link" iconPos="left" label="CSV" onClick={this.export}></Button></div>;
+    // Pagination
     let paginatorLeft = <Button icon="pi pi-refresh"/>;
     let paginatorRight = <Button icon="pi pi-cloud-upload"/>;
 
     return (
-      <DataTable value={products} paginator={true} paginatorLeft={paginatorLeft} paginatorRight={paginatorRight} rows={10} rowsPerPageOptions={[5,10,20]}>
+      <DataTable value={products} paginator={true} paginatorLeft={paginatorLeft} paginatorRight={paginatorRight} rows={10} rowsPerPageOptions={[5,10,20]} header={header} ref={(el) => { this.dt = el; }}>
           <Column field="name" header="Name" />
           <Column field="serviceCode" header="Service Code" />
           <Column field="serviceBy" header="Serviced By" />
